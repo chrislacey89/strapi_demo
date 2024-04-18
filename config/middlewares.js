7afe24a6f924dac9900b453f73930f3ef2a9e593
Chris Lacey <clacey@fusionalliance.com>
2024-04-18 16:05:44 -0400

    add middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,22 @@
-module.exports = [
-  'strapi::logger',
+module.exports  = [
   'strapi::errors',
-  'strapi::security',
+  {
+      name:  "strapi::security",
+      config:  {
+         contentSecurityPolicy:  {
+         useDefaults:  true,
+         directives:  {
+           'frame-src':["'self'","https://www.wikipedia.org/"],
+           'frame-ancestors':  null,
+           upgradeInsecureRequests:  null,
+                                 },
+                        },
+     frameguard:  false,
+               },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
